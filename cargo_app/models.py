67d028bddc70cdcaeadd1aead317f30b8915ec27
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-24 22:37:06 +0500

    add models

--- cargo_app/models.py
+++ cargo_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=10)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+
+class Cargo(models.Model):
+    pick_up_location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='pick_up_cargos')
+    delivery_location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='delivery_cargos')
+    weight = models.IntegerField()
+    description = models.TextField()
+
+
+class Car(models.Model):
+    number = models.CharField(max_length=5, unique=True)
+    current_location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='cars')
+    carrying_capacity = models.IntegerField()
