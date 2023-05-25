df23d4df02d49192c4a56245ec20181c6b8a0293
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-25 14:26:15 +0500

    fix urls

--- cargo_api/urls.py
+++ cargo_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from cargo_app.views import CargoListCreateView, CarListCreateView
+from cargo_app.views import CargoListCreateView, CargoDetailView, CarListCreateView, CarDetailView
 
 
 urlpatterns = [
     path('cargos/', CargoListCreateView.as_view(), name='cargo-list-create'),
+    path('cargos/<int:pk>/', CargoDetailView.as_view(), name='cargo-detail'),
     path('cars/', CarListCreateView.as_view(), name='car-list-create'),
+    path('cars/<int:pk>/', CarDetailView.as_view(), name='car-detail'),
 ]
