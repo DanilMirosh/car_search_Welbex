c99c734cb758febd9a6f8f2634498bd4ef12cc57
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-25 14:23:55 +0500

    fix serializers

--- cargo_app/serializers.py
+++ cargo_app/serializers.py
@@ -9,9 +9,18 @@ class LocationSerializer(serializers.ModelSerializer):
 
 
 class CargoSerializer(serializers.ModelSerializer):
+    nearby_cars_count = serializers.SerializerMethodField()
+    car_distances = serializers.SerializerMethodField()
+
     class Meta:
         model = Cargo
         fields = '__all__'
+
+    def get_nearby_cars_count(self, obj):
+        return obj.nearby_cars_count()
+
+    def get_car_distances(self, obj):
+        return obj.car_distances()
 
 
 class CarSerializer(serializers.ModelSerializer):
