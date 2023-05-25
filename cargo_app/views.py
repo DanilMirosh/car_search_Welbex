80955fe031fb446a8a99ecd28c188c4d6829eac8
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-25 14:24:57 +0500

    fix views

--- cargo_app/views.py
+++ cargo_app/views.py
@@ -8,6 +8,18 @@ class CargoListCreateView(generics.ListCreateAPIView):
     serializer_class = CargoSerializer
 
 
+class CargoDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cargo.objects.all()
+    serializer_class = CargoSerializer
+    lookup_field = 'pk'
+
+
 class CarListCreateView(generics.ListCreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+
+class CarDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    lookup_field = 'pk'
